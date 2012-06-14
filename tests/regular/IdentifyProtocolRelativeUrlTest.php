e93f1cba87feb94a94f04cb08dfa6545e8fead26
Jon Cram <jon@webignition.net>
2012-06-14 12:43:53 +0100

    Added isRelative()

---
+++ tests/regular/IdentifyProtocolRelativeUrlTest.php
@@ -0,0 +1,23 @@
+<?php
+ini_set('display_errors', 'On');
+require_once(__DIR__.'/../../lib/bootstrap.php');
+
+/**
+ * Check that relative URLs are identified as such
+ *  
+ */
+class IdentifyRelativeUrlTest extends AbstractRegularUrlTest {   
+    
+    public function testRelativeUrlIsRecognisedAsRelative() {        
+        $url = new \webignition\Url\Url('/relative/url/here');
+        
+        $this->assertTrue($url->isRelative());
+    }
+    
+    public function testAbsoluteUrlIsRecognisedAsNonRelative() {
+        $url = new \webignition\Url\Url('http://example.com/absolute/url');
+        
+        $this->assertFalse($url->isRelative());        
+    }
+    
+}
