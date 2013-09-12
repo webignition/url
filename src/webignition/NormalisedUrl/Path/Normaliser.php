f29365134d0092d766b2d4796259d3bd0fdba922
Jon Cram <jon@webignition.net>
2013-09-12 15:09:20 +0100

    Remove unused cruft

--- src/webignition/NormalisedUrl/Path/Normaliser.php
+++ src/webignition/NormalisedUrl/Path/Normaliser.php
@@ -30,8 +30,7 @@ class Normaliser {
     private function normalise() {
         $this->removeDotSegments();
         $this->reduceMultipleTrailingSlashes();
-        $this->addTrailingSlash();
-        $this->addLeadingSlash();        
+        $this->addTrailingSlash();      
     }
     
     /**
@@ -44,40 +43,10 @@ class Normaliser {
         }
         
         if ($this->path == '' || $this->path == '/') {
-            return $this->path = '/';
+            $this->path = '/';
         }
         
-
-        
-        $pathParts = explode('/', $this->path);
-        
-//        var_dump($pathParts, $this->path);
-//        exit();
-        
-        return;        
-        $lastPathPart = $pathParts[count($pathParts) - 1];
-
-        if (substr_count($lastPathPart, '.')) {
-            return;
-        }
-        
-        $lastPathCharacter = substr($this->path, strlen($this->path) - 1);
-        if ($lastPathCharacter != '/') {
-            $this->path .= '/';
-        }        
-    }
-    
-    
-    /**
-     * Prepend path with leading slash if this URL has a host and the path lacks
-     * the leading slash 
-     */
-    private function addLeadingSlash() {
-        $firstPathCharacter = substr($this->path, 0, 1);
-        
-        if (isset($this->parts['host']) && $firstPathCharacter != '/') {
-            $this->path = '/' . $this->path;
-        }
+        return;     
     }
     
     
@@ -133,23 +102,16 @@ class Normaliser {
         $trailingSlashSegmentIndex = strlen($this->path) - 1;
         $hasReachedNonSlashCharacter = false;
         
-        //var_dump($this->path);
-        
         for ($characterIndex = strlen($this->path) - 1; $characterIndex >= 0; $characterIndex--) {
             if (!$hasReachedNonSlashCharacter) {
-                //var_dump($characterIndex);
                 if ($this->path[$characterIndex] == '/') {
-                    //var_dump("beep $characterIndex");
                     $trailingSlashSegmentIndex = $characterIndex;
                 } else {
                     $hasReachedNonSlashCharacter = true;
                 }                
             }
         }
-        
-//        var_dump($trailingSlashSegmentIndex);
-//        exit();
-//        
+     
         $trailingSlashSegment = ($trailingSlashSegmentIndex == 0) ? $this->path : substr($this->path, $trailingSlashSegmentIndex);
         
         while (substr_count($trailingSlashSegment, '//')) {
@@ -157,37 +119,6 @@ class Normaliser {
         }
         
         $this->path = substr($this->path, 0, $trailingSlashSegmentIndex) . $trailingSlashSegment;
-        
-        return;
-        
-        var_dump($trailingSlashSegment);
-        
-        exit();
-        
-        
-        $currentPathParts = explode('/', $this->path);
-        $reversePathParts = array();
-        $hasFoundNonBlankPart = false;
-        
-        var_dump($currentPathParts);
-//        /exit();
-        
-        for ($partIndex = count($currentPathParts) - 1; $partIndex >= 0; $partIndex--) {            
-            $part = trim($currentPathParts[$partIndex]);
-            
-            if ($part != '') {
-                $hasFoundNonBlankPart = true;
-            }
-            
-            if ($hasFoundNonBlankPart || (!$hasFoundNonBlankPart && $part != '')) {
-                $reversePathParts[] = $part;
-            }
-        }
-        
-        var_dump(array_reverse($reversePathParts));
-        exit();
-        
-        $this->path = implode('/', array_reverse($reversePathParts)) ;
     }
     
 }
