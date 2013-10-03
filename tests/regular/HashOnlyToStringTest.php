9befb6527a42d69afde6328d685ddcede86f5b27
Jon Cram <jon@webignition.net>
2013-10-03 09:18:38 +0100

    Demonstrate that hash-only urls are parsed and stringified correctly

---
+++ tests/regular/HashOnlyToStringTest.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * 
+ */
+class HashOnlyToStringTest extends AbstractRegularUrlTest {   
+    
+    public function testHashOnlyUrlResolvesToBlankString() {         
+        $url = new \webignition\Url\Url('#');                        
+        
+        $this->assertEquals('', (string)$url);
+    }  
+    
+    public function testHashWithIdentifierResolvesToHashWithIdentifier() {         
+        $url = new \webignition\Url\Url('#foo');                        
+        
+        $this->assertEquals('#foo', (string)$url);
+    }      
+}
