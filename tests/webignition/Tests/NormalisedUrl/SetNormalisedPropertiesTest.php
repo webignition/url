d7b31fa32f3a104c15a8cb7dc6b98813d16cdb93
Jon Cram <jon@webignition.net>
2018-03-06 16:58:07 +0000

    Test NormalisedUrl/Path/Normaliser

--- tests/webignition/Tests/NormalisedUrl/SetNormalisedPropertiesTest.php
+++ tests/webignition/Tests/NormalisedUrl/SetNormalisedPropertiesTest.php
@@ -5,70 +5,70 @@ use webignition\Tests\NormalisedUrl\AbstractNormalisedUrlTest;
 
 /**
  * Check that URL properties can be set
- *  
+ *
  */
-class SetNormalisedPropertiesTest extends AbstractNormalisedUrlTest {   
-    
-    public function testSetScheme() {         
-        $url = new \webignition\NormalisedUrl\NormalisedUrl($this->urls['protocol-relative']);                        
+class SetNormalisedPropertiesTest extends AbstractNormalisedUrlTest {
+
+    public function testSetScheme() {
+        $url = new \webignition\NormalisedUrl\NormalisedUrl($this->urls['protocol-relative']);
         $url->setScheme('hTTp');
-        
-        $this->assertEquals('http', $url->getScheme());        
+
+        $this->assertEquals('http', $url->getScheme());
         $this->assertEquals($this->urls['complete'], (string)$url);
     }
-    
-    public function testSetSchemeForUrlThatHasNoHost() {         
-        $url = new \webignition\NormalisedUrl\NormalisedUrl($this->urls['root-relative']);                        
-        $url->setScheme('hTTp');
-        
-        $this->assertNull($url->getScheme());
-     }  
-   
-    public function testSetHost() {        
+
+    public function testSetSchemeForUrlThatHasNoHost() {
         $url = new \webignition\NormalisedUrl\NormalisedUrl($this->urls['root-relative']);
-                
-        $url->setHost('eXAMPle.com');        
+        $url->setScheme('hTTp');
+
+        $this->assertNull($url->getScheme());
+     }
+
+    public function testSetHost() {
+        $url = new \webignition\NormalisedUrl\NormalisedUrl($this->urls['root-relative']);
+
+        $url->setHost('eXAMPle.com');
         $this->assertEquals('example.com', $url->getHost());
         $this->assertEquals('//example.com' . $this->urls['root-relative'], (string)$url);
-    }    
-    
+    }
+
     public function testSetPath() {
         $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/');
-                
-        $url->setPath('/path/././././././././');        
+
+        $url->setPath('/path/././././././././');
         $this->assertEquals('/path/', $url->getPath());
         $this->assertEquals('http://example.com/path/', (string)$url);
-    } 
-    
-    public function testSetQueryWithNoFragmentWithoutQuestionMark() {        
-        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/');                        
-        
+    }
+
+    public function testSetQueryWithNoFragmentWithoutQuestionMark() {
+        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/');
+
         $url->setQuery('key2=value2&key1=value1');
         $this->assertEquals('key1=value1&key2=value2', (string)$url->getQuery());
-        $this->assertEquals('http://example.com/?key1=value1&key2=value2', (string)$url);      
+        $this->assertEquals('http://example.com/?key1=value1&key2=value2', (string)$url);
     }
-    
-    public function testSetQueryWithNoFragmentWithQuestionMark() {                
-        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/');                        
+
+    public function testSetQueryWithNoFragmentWithQuestionMark() {
+        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/');
 
         $url->setQuery('?key2=value2&key1=value1');
         $this->assertEquals('key1=value1&key2=value2', (string)$url->getQuery());
-        $this->assertEquals('http://example.com/?key1=value1&key2=value2', (string)$url);        
-    }    
-    
+        $this->assertEquals('http://example.com/?key1=value1&key2=value2', (string)$url);
+    }
+
     public function testSetQueryWithFragmentWithoutQuestionMark() {
-        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/#fragment');                        
-        
+        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/#fragment');
+
         $url->setQuery('key2=value2&key1=value1');
         $this->assertEquals('key1=value1&key2=value2', (string)$url->getQuery());
-        $this->assertEquals('http://example.com/?key1=value1&key2=value2#fragment', (string)$url);        
-    }    
-    
+        $this->assertEquals('http://example.com/?key1=value1&key2=value2#fragment', (string)$url);
+    }
+
     public function testSetQueryWithFragmentWithQuestionMark() {
-        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/#fragment');                        
-        
+        $url = new \webignition\NormalisedUrl\NormalisedUrl('http://example.com/#fragment');
+
         $url->setQuery('?key2=value2&key1=value1');
         $this->assertEquals('key1=value1&key2=value2', (string)$url->getQuery());
-        $this->assertEquals('http://example.com/?key1=value1&key2=value2#fragment', (string)$url);        
-    } 
+        $this->assertEquals('http://example.com/?key1=value1&key2=value2#fragment', (string)$url);
+    }
 }
