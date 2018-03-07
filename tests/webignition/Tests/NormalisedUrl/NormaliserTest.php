33f3dd0ce537936eb31fc9c9b3c755acfacdab81
Jon Cram <jon@webignition.net>
2018-03-07 07:10:30 +0000

    Test NormalisedUrl/Normaliser::normalisePort()

--- tests/webignition/Tests/NormalisedUrl/NormaliserTest.php
+++ tests/webignition/Tests/NormalisedUrl/NormaliserTest.php
@@ -13,7 +13,7 @@ class NormaliserTest extends \PHPUnit_Framework_TestCase
      * @param string $url
      * @param string $expectedNormalisedScheme
      */
-    public function testNormalisedScheme($url, $expectedNormalisedScheme)
+    public function testNormaliseScheme($url, $expectedNormalisedScheme)
     {
         $normaliser = new Normaliser($url);
 
@@ -55,4 +55,83 @@ class NormaliserTest extends \PHPUnit_Framework_TestCase
         ];
     }
 
+    /**
+     * @dataProvider normaliseHostDataProvider
+     *
+     * @param string $url
+     * @param string $expectedNormalisedHost
+     */
+    public function testNormaliseHost($url, $expectedNormalisedHost)
+    {
+        $normaliser = new Normaliser($url);
+
+        $normalisedParts = $normaliser->getParts();
+
+        $this->assertEquals($expectedNormalisedHost, (string)$normalisedParts[UrlInterface::PART_HOST]);
+    }
+
+    /**
+     * @return array
+     */
+    public function normaliseHostDataProvider()
+    {
+        return [
+            'is lowercased' => [
+                'url' => 'http://exAMPlE.com/',
+                'expectedNormalisedHost' => 'example.com',
+            ],
+            'punycode is unchanged' => [
+                'url' => 'http://artesan.xn--a-iga.com/',
+                'expectedNormalisedHost' => 'artesan.xn--a-iga.com',
+            ],
+            'utf8 is converted to punycode' => [
+                'url' => 'http://artesan.ía.com/',
+                'expectedNormalisedHost' => 'artesan.xn--a-iga.com',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider normalisePortDataProvider
+     *
+     * @param string $url
+     * @param bool $expectedPortIsSet
+     * @param string $expectedNormalisedPort
+     */
+    public function testNormalisePort($url, $expectedPortIsSet, $expectedNormalisedPort = null)
+    {
+        $normaliser = new Normaliser($url);
+
+        $normalisedParts = $normaliser->getParts();
+
+        if ($expectedPortIsSet) {
+            $this->assertEquals($expectedNormalisedPort, (string)$normalisedParts[UrlInterface::PART_PORT]);
+        } else {
+            $this->assertArrayNotHasKey(UrlInterface::PART_PORT, $normalisedParts);
+        }
+
+
+    }
+
+    /**
+     * @return array
+     */
+    public function normalisePortDataProvider()
+    {
+        return [
+            'port 80 is removed for http' => [
+                'url' => 'http://example.com:80/',
+                'expectedPortIsSet' => false,
+            ],
+            'port 443 is removed for https' => [
+                'url' => 'https://example.com:443/',
+                'expectedPortIsSet' => false,
+            ],
+            'port 8080 is not removed' => [
+                'url' => 'http://example.com:8080/',
+                'expectedPortIsSet' => true,
+                'expectedNormalisedPort' => 8080,
+            ],
+        ];
+    }
 }
