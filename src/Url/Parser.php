f700fe0bf1b62230313152306d347f37c52480bd
Jon Cram <webignition@gmail.com>
2018-03-14 17:19:34 +0000

    Fix parsing of scheme-only urls (#24)

--- src/Url/Parser.php
+++ src/Url/Parser.php
@@ -17,6 +17,14 @@ class Parser implements ParserInterface
     const PROTOCOL_RELATIVE_DUMMY_SCHEME = 'dummy';
 
     /**
+     * Scheme names consist of a sequence of characters beginning with a
+     * letter and followed by any combination of letters, digits, plus
+     * ("+"), period ("."), or hyphen ("-").
+     * @see https://tools.ietf.org/html/rfc3986#section-3.1
+     */
+    const PATTERN_SCHEME_ONLY_URL = '/^[a-z][a-z0-9+\.-]+:\/\/$/i';
+
+    /**
      * Collection of the different parts of the URL
      *
      * @var array
@@ -52,6 +60,10 @@ class Parser implements ParserInterface
 
         $parts = parse_url($url);
 
+        if (false === $parts) {
+            $parts = $this->fixFailedParse($url);
+        }
+
         if (self::FRAGMENT_SEPARATOR === substr($url, strlen($url) - 1)) {
             $parts[UrlInterface::PART_FRAGMENT] = '';
         }
@@ -83,5 +95,21 @@ class Parser implements ParserInterface
         }
 
         return $parts;
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return array
+     */
+    private function fixFailedParse($url)
+    {
+        if (preg_match(self::PATTERN_SCHEME_ONLY_URL, $url)) {
+            return [
+                'scheme' => preg_replace('/:\/\/$/', '', $url),
+            ];
+        }
+
+        return [];
     }
 }
