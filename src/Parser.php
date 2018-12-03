c372c65e470415105aea53acabc66b91033ed4a2
Jon Cram <webignition@gmail.com>
2018-12-03 15:16:16 +0000

    Parse url string with invalid port number (#57)

--- src/Parser.php
+++ src/Parser.php
@@ -4,10 +4,21 @@ namespace webignition\Url;
 
 class Parser
 {
-    const FRAGMENT_SEPARATOR = '#';
+    const QUERY_DELIMITER = '?';
+    const FRAGMENT_DELIMITER = '#';
+    const PATH_DELIMITER = '/';
 
     const PROTOCOL_RELATIVE_START = '//';
     const PROTOCOL_RELATIVE_DUMMY_SCHEME = 'dummy';
+
+    const PART_SCHEME = 'scheme';
+    const PART_USER = 'user';
+    const PART_PASS = 'pass';
+    const PART_HOST = 'host';
+    const PART_PORT = 'port';
+    const PART_PATH = 'path';
+    const PART_QUERY = 'query';
+    const PART_FRAGMENT = 'fragment';
 
     /**
      * Scheme names consist of a sequence of characters beginning with a
@@ -15,7 +26,7 @@ class Parser
      * ("+"), period ("."), or hyphen ("-").
      * @see https://tools.ietf.org/html/rfc3986#section-3.1
      */
-    const PATTERN_SCHEME_ONLY_URL = '/^[a-z][a-z0-9+\.-]+:\/\/$/i';
+    const SCHEME_ONLY_URL_PATTERN = '/^[a-z][a-z0-9+\.-]+:\/\/$/i';
 
     public function parse(string $url): array
     {
@@ -25,30 +36,29 @@ class Parser
             $url = self::PROTOCOL_RELATIVE_DUMMY_SCHEME . ':' . $url;
         }
 
-        $parts = parse_url($url);
-
+        $parts = $this->parseParts($url);
         if (false === $parts) {
-            $parts = $this->fixFailedParse($url);
+            return [];
         }
 
-        if (self::FRAGMENT_SEPARATOR === substr($url, strlen($url) - 1)) {
-            $parts[UrlInterface::PART_FRAGMENT] = '';
+        if (strlen($url) && self::FRAGMENT_DELIMITER === $url[-1]) {
+            $parts[self::PART_FRAGMENT] = '';
         }
 
         $scheme = isset($parts[UrlInterface::PART_SCHEME])
-            ? $parts[UrlInterface::PART_SCHEME]
+            ? $parts[self::PART_SCHEME]
             : null;
 
         if (self::PROTOCOL_RELATIVE_DUMMY_SCHEME === $scheme) {
-            unset($parts[UrlInterface::PART_SCHEME]);
+            unset($parts[self::PART_SCHEME]);
         }
 
-        if (isset($parts[UrlInterface::PART_PORT])) {
-            $parts[UrlInterface::PART_PORT] = (int)$parts[UrlInterface::PART_PORT];
+        if (isset($parts[self::PART_PORT])) {
+            $parts[self::PART_PORT] = (int)$parts[self::PART_PORT];
         }
 
-        if (isset($parts[UrlInterface::PART_PATH]) && empty($parts[UrlInterface::PART_PATH])) {
-            unset($parts[UrlInterface::PART_PATH]);
+        if (isset($parts[self::PART_PATH]) && empty($parts[UrlInterface::PART_PATH])) {
+            unset($parts[self::PART_PATH]);
         }
 
         return $parts;
@@ -69,12 +79,131 @@ class Parser
         return $url;
     }
 
-    private function fixFailedParse(?string $url): array
+    private function parseParts(string $url): array
     {
-        if (preg_match(self::PATTERN_SCHEME_ONLY_URL, $url)) {
+        $parts = parse_url($url);
+
+        if (false !== $parts) {
+            return $parts;
+        }
+
+        $parts = $this->parseUrlWithOnlyScheme($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        $parts = $this->parseUrlWithInvalidPort($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        return [];
+    }
+
+    private function parseUrlWithOnlyScheme(string $url): array
+    {
+        if (preg_match(self::SCHEME_ONLY_URL_PATTERN, $url)) {
             return [
                 'scheme' => preg_replace('/:\/\/$/', '', $url),
             ];
+        }
+
+        return [];
+    }
+
+    private function parseUrlWithInvalidPort(string $url): array
+    {
+        $parts = $this->parseUrlWithInvalidPortWithPath($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        $parts = $this->parseUrlWithInvalidPortWithoutPathWithQuery($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        $parts = $this->parseUrlWithInvalidPOrtWithoutPathWithoutQueryWithFragment($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        $parts = $this->parseUrlEndingWithPortPattern($url);
+        if (!empty($parts)) {
+            return $parts;
+        }
+
+        return [];
+    }
+
+    private function parseUrlWithInvalidPortWithPath(string $url): array
+    {
+        $doubleSlashPosition = strpos($url, '//');
+
+        $firstSlashSearchOffset = false === $doubleSlashPosition
+            ? 0
+            : $doubleSlashPosition + 2;
+
+        $firstSlashPosition = strpos($url, '/', $firstSlashSearchOffset);
+
+        if (false === $firstSlashPosition) {
+            return [];
+        }
+
+        return $this->parseUrlEndingWithPortPatternAndSuffix($url, $firstSlashPosition);
+    }
+
+    private function parseUrlWithInvalidPortWithoutPathWithQuery(string $url): array
+    {
+        $queryDelimiterPosition = strpos($url, self::QUERY_DELIMITER);
+
+        if (false === $queryDelimiterPosition) {
+            return [];
+        }
+
+        $fragmentDelimiterPosition = strpos($url, self::FRAGMENT_DELIMITER);
+
+        if (false !== $fragmentDelimiterPosition && $fragmentDelimiterPosition < $queryDelimiterPosition) {
+            return [];
+        }
+
+        return $this->parseUrlEndingWithPortPatternAndSuffix($url, $queryDelimiterPosition);
+    }
+
+    private function parseUrlWithInvalidPOrtWithoutPathWithoutQueryWithFragment(string $url): array
+    {
+        $fragmentDelimiterPosition = strpos($url, self::FRAGMENT_DELIMITER);
+
+        if (false === $fragmentDelimiterPosition) {
+            return [];
+        }
+
+        return $this->parseUrlEndingWithPortPatternAndSuffix($url, $fragmentDelimiterPosition);
+    }
+
+    private function parseUrlEndingWithPortPatternAndSuffix(string $url, int $suffixPosition)
+    {
+        $urlEndingWithPortPattern = substr($url, 0, $suffixPosition);
+        $suffix = substr($url, $suffixPosition);
+
+        return $this->parseUrlEndingWithPortPattern($urlEndingWithPortPattern, $suffix);
+    }
+
+    private function parseUrlEndingWithPortPattern(string $urlEndingWithPortPattern, string $postPortSuffix = '')
+    {
+        $endsWithPortPattern = '/\:[0-9]+$/';
+        $endsWithPortMatches = [];
+
+        if (preg_match($endsWithPortPattern, $urlEndingWithPortPattern, $endsWithPortMatches) > 0) {
+            $modifiedUrl = preg_replace($endsWithPortPattern, '', $urlEndingWithPortPattern);
+            $port = str_replace(':', '', $endsWithPortMatches[0]);
+
+            $modifiedUrl .= $postPortSuffix;
+
+            $parts = parse_url($modifiedUrl);
+            $parts[self::PART_PORT] = $port;
+
+            return $parts;
         }
 
         return [];
--- tests/ParserTest.php
+++ tests/ParserTest.php
@@ -3,7 +3,6 @@
 namespace webignition\Url\Tests;
 
 use webignition\Url\Parser;
-use webignition\Url\UrlInterface;
 
 class ParserTest extends \PHPUnit\Framework\TestCase
 {
@@ -21,7 +20,8 @@ class ParserTest extends \PHPUnit\Framework\TestCase
 
     /**
      * @dataProvider parseDataProvider
-     * @dataProvider normalizeWhitespaceDataProvider
+     * dataProvider normalizeWhitespaceDataProvider
+     * dataProvider invalidPortDataProvider
      *
      * @param string $url
      * @param array $expectedParts
@@ -30,11 +30,7 @@ class ParserTest extends \PHPUnit\Framework\TestCase
     {
         $parts = $this->parser->parse($url);
 
-        $this->assertEquals(array_keys($expectedParts), array_keys($parts));
-
-        foreach ($expectedParts as $key => $value) {
-            $this->assertEquals($expectedParts[$key], $parts[$key]);
-        }
+        $this->assertEquals($expectedParts, $parts);
     }
 
     public function parseDataProvider(): array
@@ -47,148 +43,154 @@ class ParserTest extends \PHPUnit\Framework\TestCase
             'complete fully qualified' => [
                 'url' => 'http://user:pass@example.com:8080/path1/path2/filename.extension?foo=bar#fragment',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PORT => 8080,
-                    UrlInterface::PART_USER => 'user',
-                    UrlInterface::PART_PASS => 'pass',
-                    UrlInterface::PART_PATH => '/path1/path2/filename.extension',
-                    UrlInterface::PART_QUERY => 'foo=bar',
-                    UrlInterface::PART_FRAGMENT => 'fragment',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => 8080,
+                    Parser::PART_USER => 'user',
+                    Parser::PART_PASS => 'pass',
+                    Parser::PART_PATH => '/path1/path2/filename.extension',
+                    Parser::PART_QUERY => 'foo=bar',
+                    Parser::PART_FRAGMENT => 'fragment',
                 ],
             ],
             'complete protocol-relative' => [
                 'url' => '//user:pass@example.com:8080/path1/path2/filename.extension?foo=bar#fragment',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PORT => 8080,
-                    UrlInterface::PART_USER => 'user',
-                    UrlInterface::PART_PASS => 'pass',
-                    UrlInterface::PART_PATH => '/path1/path2/filename.extension',
-                    UrlInterface::PART_QUERY => 'foo=bar',
-                    UrlInterface::PART_FRAGMENT => 'fragment',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => 8080,
+                    Parser::PART_USER => 'user',
+                    Parser::PART_PASS => 'pass',
+                    Parser::PART_PATH => '/path1/path2/filename.extension',
+                    Parser::PART_QUERY => 'foo=bar',
+                    Parser::PART_FRAGMENT => 'fragment',
                 ],
             ],
             'root relative' => [
                 'url' => '/path1/path2/filename.extension?foo=bar#fragment',
                 'expectedParts' => [
-                    UrlInterface::PART_PATH => '/path1/path2/filename.extension',
-                    UrlInterface::PART_QUERY => 'foo=bar',
-                    UrlInterface::PART_FRAGMENT => 'fragment',
+                    Parser::PART_PATH => '/path1/path2/filename.extension',
+                    Parser::PART_QUERY => 'foo=bar',
+                    Parser::PART_FRAGMENT => 'fragment',
                 ],
             ],
             'relative' => [
                 'url' => 'path1/path2/filename.extension?foo=bar#fragment',
                 'expectedParts' => [
-                    UrlInterface::PART_PATH => 'path1/path2/filename.extension',
-                    UrlInterface::PART_QUERY => 'foo=bar',
-                    UrlInterface::PART_FRAGMENT => 'fragment',
+                    Parser::PART_PATH => 'path1/path2/filename.extension',
+                    Parser::PART_QUERY => 'foo=bar',
+                    Parser::PART_FRAGMENT => 'fragment',
                 ],
             ],
             'hash only' => [
                 'url' => '#',
                 'expectedParts' => [
-                    UrlInterface::PART_FRAGMENT => '',
+                    Parser::PART_FRAGMENT => '',
                 ],
             ],
             'path and hash only' => [
                 'url' => '/index.html#',
                 'expectedParts' => [
-                    UrlInterface::PART_PATH => '/index.html',
-                    UrlInterface::PART_FRAGMENT => '',
+                    Parser::PART_PATH => '/index.html',
+                    Parser::PART_FRAGMENT => '',
                 ],
             ],
             'hash and identifier only' => [
                 'url' => '#fragment',
                 'expectedParts' => [
-                    UrlInterface::PART_FRAGMENT => 'fragment',
+                    Parser::PART_FRAGMENT => 'fragment',
                 ],
             ],
             'scheme, no username, no password' => [
                 'url' => 'https://@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'https',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
+                    Parser::PART_SCHEME => 'https',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
                 ],
             ],
             'protocol-relative, no username, no password' => [
                 'url' => '//@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
                 ],
             ],
             'scheme, empty username, empty password' => [
                 'url' => 'https://:@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'https',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
-                    UrlInterface::PART_PASS => '',
+                    Parser::PART_SCHEME => 'https',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
+                    Parser::PART_PASS => '',
                 ],
             ],
             'protocol-relative, empty username, empty password' => [
                 'url' => '//:@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
-                    UrlInterface::PART_PASS => '',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
+                    Parser::PART_PASS => '',
                 ],
             ],
             'scheme, empty username, has password' => [
                 'url' => 'https://:password@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'https',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
-                    UrlInterface::PART_PASS => 'password',
+                    Parser::PART_SCHEME => 'https',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
+                    Parser::PART_PASS => 'password',
                 ],
             ],
             'protocol-relative, empty username, has password' => [
                 'url' => '//:password@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => '',
-                    UrlInterface::PART_PASS => 'password',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => '',
+                    Parser::PART_PASS => 'password',
                 ],
             ],
             'scheme, has username, empty password' => [
                 'url' => 'https://username:@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'https',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => 'username',
-                    UrlInterface::PART_PASS => '',
+                    Parser::PART_SCHEME => 'https',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => 'username',
+                    Parser::PART_PASS => '',
                 ],
             ],
             'protocol-relative, has username, empty password' => [
                 'url' => '//username:@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => 'username',
-                    UrlInterface::PART_PASS => '',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => 'username',
+                    Parser::PART_PASS => '',
                 ],
             ],
             'scheme, has username, no password' => [
                 'url' => 'https://username@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'https',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => 'username',
+                    Parser::PART_SCHEME => 'https',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => 'username',
                 ],
             ],
             'protocol-relative, has username, no password' => [
                 'url' => '//username@example.com',
                 'expectedParts' => [
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_USER => 'username',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_USER => 'username',
                 ],
             ],
-            'scheme-only' => [
+            'scheme-only (file_' => [
                 'url' => 'file://',
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME  => 'file',
+                    Parser::PART_SCHEME  => 'file',
+                ],
+            ],
+            'scheme-only (http)' => [
+                'url' => 'http://',
+                'expectedParts' => [
+                    Parser::PART_SCHEME  => 'http',
                 ],
             ],
         ];
@@ -200,75 +202,142 @@ class ParserTest extends \PHPUnit\Framework\TestCase
             'trailing tab is removed' => [
                 'url' => "http://example.com\t",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'trailing newline is removed' => [
                 'url' => "http://example.com\n",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'trailing line return' => [
                 'url' => "http://example.com\r",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'leading tab is removed' => [
                 'url' => "\thttp://example.com",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'leading newline is removed' => [
                 'url' => "\nhttp://example.com",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'leading line return' => [
                 'url' => "\nhttp://example.com",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
                 ],
             ],
             'tab in path is removed' => [
                 'url' => "http://example.com/foo\t/bar",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PATH => '/foo/bar',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PATH => '/foo/bar',
                 ],
             ],
             'newline in path is removed' => [
                 'url' => "http://example.com/foo\n/bar",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PATH => '/foo/bar',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PATH => '/foo/bar',
                 ],
             ],
             'line return in path is removed' => [
                 'url' => "http://example.com/foo\r/bar",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PATH => '/foo/bar',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PATH => '/foo/bar',
                 ],
             ],
             'many tabs, newlines and line returns' => [
                 'url' => "\n\thttp://example.com\r\n/\rpage/\t",
                 'expectedParts' => [
-                    UrlInterface::PART_SCHEME => 'http',
-                    UrlInterface::PART_HOST => 'example.com',
-                    UrlInterface::PART_PATH => '/page/',
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PATH => '/page/',
+                ],
+            ],
+        ];
+    }
+
+    public function invalidPortDataProvider(): array
+    {
+        return [
+            'invalid port (too small), no path' => [
+                'url' => 'http://example.com:0',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                ],
+            ],
+            'invalid port (too large), no path' => [
+                'url' => 'http://example.com:65536',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '65536',
+                ],
+            ],
+            'invalid port (too small), with path' => [
+                'url' => 'http://example.com:0/path',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                    Parser::PART_PATH => '/path',
+                ],
+            ],
+            'invalid port (too small), with path containing port-like pattern' => [
+                'url' => 'http://example.com:0/path:0/path',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                    Parser::PART_PATH => '/path:0/path',
+                ],
+            ],
+            'invalid port (too small), with query containing port-like pattern' => [
+                'url' => 'http://example.com:0?:0',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                    Parser::PART_QUERY => ':0',
+                ],
+            ],
+            'invalid port (too small), with fragment containing port-like pattern' => [
+                'url' => 'http://example.com:0#:0',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                    Parser::PART_FRAGMENT => ':0',
+                ],
+            ],
+            'invalid port (too small), with fragment containing port-like pattern and query-like pattern' => [
+                'url' => 'http://example.com:0#:0?:0',
+                'expectedParts' => [
+                    Parser::PART_SCHEME => 'http',
+                    Parser::PART_HOST => 'example.com',
+                    Parser::PART_PORT => '0',
+                    Parser::PART_FRAGMENT => ':0?:0',
                 ],
             ],
         ];
