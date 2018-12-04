e5e5a8f18130eacf6421becef9109ffc894671c2
Jon Cram <webignition@gmail.com>
2018-12-04 14:30:34 +0000

    Integrate Normalizer with Uri (#61)

--- src/NormalizerOptions.php
+++ src/NormalizerOptions.php
@@ -12,7 +12,6 @@ class NormalizerOptions
     const OPTION_CONVERT_UNICODE_TO_PUNYCODE = 'convert-unicode-to-punycode';
     const OPTION_REMOVE_FRAGMENT = 'remove-fragment';
     const OPTION_REMOVE_WWW = 'remove-www';
-    const OPTION_REMOVE_KNOWN_PORTS = 'remove-known-ports';
     const OPTION_REMOVE_DEFAULT_FILES_PATTERNS = 'remove-default-files-patterns';
     const OPTION_REMOVE_PATH_DOT_SEGMENTS = 'remove-path-dot-segments';
     const OPTION_ADD_PATH_TRAILING_SLASH = 'add-path-trailing-slash';
@@ -26,7 +25,6 @@ class NormalizerOptions
     const DEFAULT_CONVERT_UNICODE_TO_PUNYCODE = true;
     const DEFAULT_REMOVE_FRAGMENT = false;
     const DEFAULT_REMOVE_WWW = false;
-    const DEFAULT_REMOVE_KNOWN_PORTS = false;
     const DEFAULT_REMOVE_PATH_DOT_SEGMENTS = false;
     const DEFAULT_ADD_PATH_TRAILING_SLASH = false;
     const DEFAULT_SORT_QUERY_PARAMETERS = false;
@@ -73,11 +71,6 @@ class NormalizerOptions
      * @var bool
      */
     private $removeWww;
-
-    /**
-     * @var bool
-     */
-    private $removeKnownPorts;
 
     /**
      * @var string[]
@@ -134,9 +127,6 @@ class NormalizerOptions
 
         $this->removeWww = $options[self::OPTION_REMOVE_WWW] ?? self::DEFAULT_REMOVE_WWW;
         $this->removeWww = (bool) $this->removeWww;
-
-        $this->removeKnownPorts = $options[self::OPTION_REMOVE_KNOWN_PORTS] ?? self::DEFAULT_REMOVE_KNOWN_PORTS;
-        $this->removeKnownPorts = (bool) $this->removeKnownPorts;
 
         $removeDefaultFilesPatterns = $options[self::OPTION_REMOVE_DEFAULT_FILES_PATTERNS] ?? null;
 
@@ -195,11 +185,6 @@ class NormalizerOptions
     public function getRemoveWww(): bool
     {
         return $this->removeWww;
-    }
-
-    public function getRemoveKnownPorts(): bool
-    {
-        return $this->removeKnownPorts;
     }
 
     /**
