5c7e8dfa3de24554c9ce4e2f3f65d7756e276c9c
Jon Cram <webignition@gmail.com>
2018-12-10 15:37:10 +0000

    Add Inspector::isProtocolRelative() (#129)

--- tests/InspectorTest.php
+++ tests/InspectorTest.php
@@ -51,4 +51,33 @@ class InspectorTest extends \PHPUnit\Framework\TestCase
             ],
         ];
     }
+
+    /**
+     * @dataProvider isProtocolRelativeDataProvider
+     *
+     * @param UriInterface $uri
+     * @param bool $expectedIsProtocolRelative
+     */
+    public function testIsProtocolRelative(UriInterface $uri, bool $expectedIsProtocolRelative)
+    {
+        $this->assertSame($expectedIsProtocolRelative, Inspector::isProtocolRelative($uri));
+    }
+
+    public function isProtocolRelativeDataProvider(): array
+    {
+        return [
+            'no scheme, no host is not protocol relative' => [
+                'uri' => new Url('/path'),
+                'expectedIsProtocolRelative' => false,
+            ],
+            'has scheme is not protocol relative' => [
+                'uri' => new Url('http://example'),
+                'expectedIsProtocolRelative' => false,
+            ],
+            'no scheme has host is protocol relative' => [
+                'uri' => new Url('//example.com'),
+                'expectedIsProtocolRelative' => true,
+            ],
+        ];
+    }
 }
