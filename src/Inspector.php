983e186a1e43301ee12cadde8db094176bbe7089
Jon Cram <jon@webignition.net>
2018-12-10 14:02:13 +0000

    Re-implement Inspector::isPubliclyRoutable() as isNotPubliclyRoutable()

--- src/Inspector.php
+++ src/Inspector.php
@@ -8,40 +8,43 @@ use Psr\Http\Message\UriInterface;
 class Inspector
 {
     /**
+     * Can a URL fail to be accessed over the public internet?
+     *
+     * There are certain cases where we can be certain that a URL cannot be accessed over the public internet:
+     *
+     * - no host
+     * - having a host that is not publicly routable (such as a within a private IP range)
+     * - having a hostname lacking dots
+     * - having a hostname starting or ending with a dot
+     *
      * @param UriInterface $url
      *
      * @return bool
      *
      * @throws InvalidExpressionException
      */
-    public function isPubliclyRoutable(UriInterface $url): bool
+    public function isNotPubliclyRoutable(UriInterface $url): bool
     {
         $host = $url->getHost();
-        if (empty($host)) {
-            return false;
+        if ('' === $host) {
+            return true;
         }
 
         $hostObject = new Host($host);
 
         if (!$hostObject->isPubliclyRoutable()) {
-            return false;
+            return true;
         }
 
         $hostContainsDots = substr_count($host, '.');
         if (!$hostContainsDots) {
-            return false;
+            return true;
         }
 
-        $hostStartsWithDot = strpos($host, '.') === 0;
-        if ($hostStartsWithDot) {
-            return false;
+        if ('.' === $host[0] || '.' === $host[-1]) {
+            return true;
         }
 
-        $hostEndsWithDot = strpos($host, '.') === strlen($host) - 1;
-        if ($hostEndsWithDot) {
-            return false;
-        }
-
-        return true;
+        return false;
     }
 }
