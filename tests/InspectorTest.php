418848f886b1bf30ad923cc467e45d6d15fcbd11
Jon Cram <webignition@gmail.com>
2018-12-10 14:08:58 +0000

    Re-implement Inspector::isPubliclyRoutable() as isNotPubliclyRoutable() (#122)

--- tests/InspectorTest.php
+++ tests/InspectorTest.php
@@ -10,46 +10,46 @@ use webignition\Url\Url;
 class InspectorTest extends \PHPUnit\Framework\TestCase
 {
     /**
-     * @dataProvider isPubliclyRoutableDataProvider
+     * @dataProvider isNotPubliclyRoutableDataProvider
      *
      * @param UriInterface $url
      * @param bool $expectedIsPubliclyRoutable
      *
      * @throws InvalidExpressionException
      */
-    public function testIsPubliclyRoutable(UriInterface $url, bool $expectedIsPubliclyRoutable)
+    public function testIsNotPubliclyRoutable(UriInterface $url, bool $expectedIsPubliclyRoutable)
     {
         $inspector = new Inspector();
 
-        $this->assertEquals($expectedIsPubliclyRoutable, $inspector->isPubliclyRoutable($url));
+        $this->assertEquals($expectedIsPubliclyRoutable, $inspector->isNotPubliclyRoutable($url));
     }
 
-    public function isPubliclyRoutableDataProvider(): array
+    public function isNotPubliclyRoutableDataProvider(): array
     {
         return [
             'no host' => [
                 'url' => new Url('example'),
-                'expectedIsPubliclyRoutable' => false,
+                'expectedIsPubliclyRoutable' => true,
             ],
             'host not publicly routable' => [
                 'url' => new Url('http://127.0.0.1'),
-                'expectedIsPubliclyRoutable' => false,
+                'expectedIsPubliclyRoutable' => true,
             ],
             'host lacks dots' => [
                 'url' => new Url('http://example'),
-                'expectedIsPubliclyRoutable' => false,
+                'expectedIsPubliclyRoutable' => true,
             ],
             'host starts with dot' => [
                 'url' => new Url('http://.example'),
-                'expectedIsPubliclyRoutable' => false,
+                'expectedIsPubliclyRoutable' => true,
             ],
             'host ends with dot' => [
                 'url' => new Url('http://example.'),
-                'expectedIsPubliclyRoutable' => false,
+                'expectedIsPubliclyRoutable' => true,
             ],
             'valid' => [
                 'url' => new Url('http://example.com'),
-                'expectedIsPubliclyRoutable' => true,
+                'expectedIsPubliclyRoutable' => false,
             ],
         ];
     }
